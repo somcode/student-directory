6be4d4efa32755a020718c016c3d9e74b1afc50f
Somayeh <mail.somayeh@gmail.com>
2018-02-13 11:12:39 +0000

    Replacing arrays with hashes

--- directory.rb
+++ directory.rb
@@ -1,16 +1,16 @@
-# lets put all student into an array
+# lets put all student into a hash
 students = [
-  ["DR. Hanibal Lecter" ,:november],
-  ["Darth Vader" ,:november],
-  ["Nurse Ratched" ,:november],
-  ["Michael Corleone" ,:november],
-  ["Alex DeLarge" ,:november],
-  ["The Wicked Witch of the West" ,:november],
-  ["Terminator" ,:november],
-  ["Freddy Krueger" ,:november],
-  ["The Joker" ,:november],
-  ["Joffrey Baratheon" ,:november],
-  ["Norman Bates" ,:november]
+  {name: "DR. Hanibal Lecter" ,cohort: :november},
+  {name: "Darth Vader" ,cohort: :november},
+  {name: "Nurse Ratched" ,cohort: :november},
+  {name: "Michael Corleone" ,cohort: :november},
+  {name: "Alex DeLarge" ,cohort: :november},
+  {name: "The Wicked Witch of the West" ,cohort: :november},
+  {name: "Terminator" ,cohort: :november},
+  {name: "Freddy Krueger" ,cohort: :november},
+  {name: "The Joker" ,cohort: :november},
+  {name: "Joffrey Baratheon" ,cohort: :november},
+  {name: "Norman Bates" ,cohort: :november}
 ]
 def print_header
 puts "The students of Villains Academy"
@@ -18,7 +18,7 @@ puts "-------------"
 end
 def print(names)
   names.each do |name|
-    puts "#{name[0]}  (#{name[1]} cohort)"
+    puts "#{name[:name]}  (#{name[:cohort]} cohort)"
   end
 end
 def print_footer(names)
