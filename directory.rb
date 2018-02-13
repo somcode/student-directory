e4a99be31719329229c851c4e00177e0c83dea99
Somayeh <mail.somayeh@gmail.com>
2018-02-13 10:21:57 +0000

    Extracting the code into methods and calling them

--- directory.rb
+++ directory.rb
@@ -12,11 +12,19 @@ students = [
   "Joffrey Baratheon" ,
   "Norman Bates"
 ]
-# and then print them
+def print_header
 puts "The students of Villains Academy"
 puts "-------------"
-students.each do |student|
-  puts student
 end
-#finally, we print the total
-puts "Overall, we have #{students.count} great students"
+def print(names)
+  names.each do |name|
+    puts name
+  end
+end
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+# nothings happen untill we call tne methods
+print_header
+print(students)
+print_footer(students)
