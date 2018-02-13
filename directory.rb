1808df1f538c7acc5ff28a7db1b9af80eb220d88
Somayeh <mail.somayeh@gmail.com>
2018-02-13 12:17:08 +0000

    Asking for user input instead of hardcoding the list of students

--- directory.rb
+++ directory.rb
@@ -1,30 +1,32 @@
-# lets put all student into a hash
-students = [
-  {name: "DR. Hanibal Lecter" ,cohort: :november},
-  {name: "Darth Vader" ,cohort: :november},
-  {name: "Nurse Ratched" ,cohort: :november},
-  {name: "Michael Corleone" ,cohort: :november},
-  {name: "Alex DeLarge" ,cohort: :november},
-  {name: "The Wicked Witch of the West" ,cohort: :november},
-  {name: "Terminator" ,cohort: :november},
-  {name: "Freddy Krueger" ,cohort: :november},
-  {name: "The Joker" ,cohort: :november},
-  {name: "Joffrey Baratheon" ,cohort: :november},
-  {name: "Norman Bates" ,cohort: :november}
-]
+def input_students
+  puts "Please enter the name of the students"
+  puts "To finish, just hit return twice"
+  students = []
+  name = gets.chomp
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
+end
+
 def print_header
-puts "The students of Villains Academy"
+puts "The students of my cohort at Makers Academy"
 puts "-------------"
 end
+
 def print(names)
   names.each do |name|
-    puts "#{name[:name]}  (#{name[:cohort]} cohort)"
+    puts "#{name[:name]}  (november cohort)"
   end
 end
+
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
-# nothings happen untill we call tne methods
+
+students = input_students
 print_header
 print(students)
 print_footer(students)
